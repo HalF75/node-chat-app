8462ed63c1a7906e316b3a7013b677c4fc8c0fde
HalF75 <ricardo.rcarvalho@gmail.com>
2018-08-21 15:37:15 +0200

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on("connect", function() {
   console.log("Connected to server");
 
-  socket.emit('createMessage', {
-      to: 'Ricardo',
-      text: 'That is good'
-  });
 });
 
 socket.on("disconnect", function() {
--- server/server.js
+++ server/server.js
@@ -16,14 +16,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Jen',
-        text: 'I know, right?',
-        createdAt: 0
-    });
-
     socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
+        //emit event to all clients
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+        // console.log('createMessage', message);
     });
 
     socket.on('disconnect', () => {
